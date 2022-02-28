e9803c5a1857d9459873cd37b2850e8b2ed98dba
frankemon <70698241+frankemon@users.noreply.github.com>
2022-02-28 18:54:43 +0100

    auth middleware

--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -7,11 +7,13 @@ const {
   deleteGoal,
 } = require("../controllers/goals");
 
+const { protect } = require("../middleware/auth");
+
 // @desc  Combine same routes into one chained route
 
 // router.get("/", getGoals);
 // router.post("/", setGoal);
-router.route("/").get(getGoals).post(setGoal);
+router.route("/").get(protect, getGoals).post(setGoal);
 
 // router.put("/:id", updateGoal);
 // router.delete("/:id", deleteGoal);
