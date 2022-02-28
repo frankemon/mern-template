387cf1b4566408619179c9a03db84500f261aa73
frankemon <70698241+frankemon@users.noreply.github.com>
2022-02-28 15:40:07 +0100

    mongoose rest

---
+++ backend/model/goal.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+  },
+  {
+    // Automatically adds timestamps
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Goal", goalSchema);
